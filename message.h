f6e111be75f0989ab81154e4b26678ede4ff201a
Natanlimap <natanlimap@gmail.com>
2020-06-22 10:48:13 -0300

    fixed

--- message.h
+++ message.h
@@ -4,18 +4,32 @@
 #include <string.h>
 #include <list>
 
+struct Date
+{
+	int year;
+	int month;
+	int day;
+};
+
+struct Time
+{
+	int hour;
+	int minute;
+	int second;
+};
+
 struct Message{
-	std::string m_date;
-	std::string m_time;
-	std::string m_message;
+	Date date;
+	Time time;
+	std::string message;
 	bool dayExists(const Message);
 	bool compare_dates(const Message &_lmessage);
 }
 
 struct Diary{
-	std::list<Message> m_messages;
-	std::fstream m_file;
-	std::string m_name;
+	std::list<Message> messages;
+	std::fstream file;
+	std::string name;
 	bool diaryExist(); 
 }
 
